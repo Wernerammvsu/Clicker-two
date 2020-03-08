88afc83d3f7ca9d8645ab90a2d737d35efc0de6e
Екатерина <wernerammvsu@yandex.ru>
2020-03-08 20:50:55 +0300

    Залит проект с минимальным функционалом

---
+++ clicker/Assets/Scripts/Game.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public Text scoreText;
+    private int score=0;
+    public void ClickHere() {
+        score += 1;
+        scoreText.text = "Score:  " + score;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
